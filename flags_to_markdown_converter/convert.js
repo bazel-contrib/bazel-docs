bab7c9cab539f9b747f657a58c338c00c6577ad9
Alex Eagle <alex@aspect.dev>
2025-11-13 08:45:51 -0800

    organize more

--- flags_to_markdown_converter/convert.js
+++ flags_to_markdown_converter/convert.js
@@ -17,26 +17,77 @@ for await (const chunk of process.stdin) {
 }
 
 const flags = fromBinary(FlagCollectionSchema, Buffer.from(base64String.trim(), 'base64'));
+const documentedFlags = flags.flagInfos.filter(f => f.documentationCategory !== 'UNDOCUMENTED');
+const flagsByCategory = documentedFlags.reduce((m, f) => {
+    if (f.metadataTags.length > 1) throw Error();
+    // Invent a metadata tag for flags that would otherwise be ungrouped.
+    // This is done to avoid having some nested more deeply than others.
+    let tag = f.metadataTags[0] || 'STABLE';
+    // bugfix?
+    if (f.name.startsWith('experimental_')) {
+        tag = 'EXPERIMENTAL'
+    }
+    if (!m.has(f.documentationCategory)) {
+        m.set(f.documentationCategory, new Map());
+    }
+    if (!m.get(f.documentationCategory).has(tag)) {
+        m.get(f.documentationCategory).set(tag, []);
+    }
+    m.get(f.documentationCategory).get(tag).push(f);
+    return m;
+}, new Map());
 
 // Write header
 writeLine('---');
 writeLine('title: Bazel flags');
 writeLine('---');
-writeLine('');
 
 // Write each flag
-for (const flag of flags.flagInfos) {
-    const abbrev = flag.abbreviation ? `(-${flag.abbreviation})` : '';
-    writeLine(`## --${flag.name} ${abbrev}`);
-    writeLine('');
-    // TODO: add history - what version of Bazel introduced this flag?
-    // TODO: this is wrapped in a <pre> tag to avoid invalid markdown like bare html tags
-    writeLine(escapeHtml(flag.documentation));
-    
-    if (flag.commands.length > 0) {
-        writeLine('');
-        writeLine(`_May apply to commands: ${flag.commands.join(', ')}_`);
+for (const category of flagsByCategory.keys()) {
+    writeLine(`## ${category.toLowerCase()}\n`)
+    for (const tag of flagsByCategory.get(category).keys()) {
+        writeLine(`### ${tag.toLowerCase()}\n`)
+        for (const flag of flagsByCategory.get(category).get(tag)) {
+            writeLine(`#### --${flag.name}`);
+            let aliases = [];
+            if (flag.abbreviation) {
+                aliases.push(`-${flag.abbreviation}`);
+            }
+            if (flag.hasNegativeFlag) {
+                aliases.push(`--no${flag.name}`);
+            }
+            if (flag.oldName) {
+                aliases.push(`previously ${flag.oldName}`);
+            }
+            if (flag.deprecationWarning) {
+                writeLine('[WARN] deprecated: ' + flag.deprecationWarning)
+            }
+            if (flag.optionExpansions.length > 0) {
+                writeLine('expands to ' + flag.optionExpansions)
+            }
+            if (flag.enumValues.length > 0) {
+                writeLine('enum values: ' + flag.enumValues)
+            }
+            
+            writeLine('effect: ' + flag.effectTags)
+            writeLine('allowsMultiple: ' + flag.allowsMultiple)
+            writeLine('requiresValue: ' + flag.requiresValue)
+            writeLine('defaultValue: ' + flag.defaultValue)
+
+            // TODO: add history - what version of Bazel introduced this flag?
+            // TODO: link to GitHub issue where the flag is being managed (i.e. graduate from experimental)
+            // TODO: link to definition of the flag in Bazel sources
+            // TODO: add popularity - how many times does it appear in github search for bazelrc files?
+            // TODO: this is wrapped in a <pre> tag to avoid invalid markdown like bare html tags
+            // also a bugfix for Bazel/Blaze placeholder.
+            writeLine(escapeHtml(flag.documentation.replace('%{product}', 'Bazel')));
+            
+            if (flag.commands.length > 0) {
+                writeLine('');
+                writeLine(`_May apply to commands: ${flag.commands.join(', ')}_`);
+            }
+            
+            writeLine('');
+        }
     }
-    
-    writeLine('');
 }
